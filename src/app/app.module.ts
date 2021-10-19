a9283c44e533f859cd0cd7d46c5188c68a3d1612
Majestr32 <61013205+Majestr32@users.noreply.github.com>
2021-10-19 22:00:05 +0300

    Almost ready for production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse'
 import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,6 +13,7 @@ import { Loader } from '@googlemaps/js-api-loader';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { ServicesComponent } from './services/services.component';
 import { DoctorsComponent } from './doctors/doctors.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -24,13 +26,14 @@ import { DoctorsComponent } from './doctors/doctors.component';
     DoctorsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAo28QNlDAdLBGkOYV0xI-j5GQJeRST1IM'
     }),
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
