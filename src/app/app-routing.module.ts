466e5ae1316022f2a71c47704e8a7f165b2b1426
Majestr32 <61013205+Majestr32@users.noreply.github.com>
2021-10-20 11:51:34 +0300

    Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', initialNavigation: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', initialNavigation: 'enabled', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
